75328c6b5d5f1270d2ab22a9c4b4e7168ca01e70
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2020-04-12 19:52:48 -0500

    added view and CanvasView and constrants

---
+++ Cartesian/CanvasView.swift
@@ -0,0 +1,18 @@
+//
+//  CanvasView.swift
+//  Cartesian
+//
+//  Created by Grace Huang on 4/12/20.
+//  Copyright © 2020 Grace Huang. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    
+
+}
